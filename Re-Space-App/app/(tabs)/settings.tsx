88706bf2e0e9dfbfc19c12dd7e83071ed2d3449d
ALEX PURSER <AP276620@falmouth.ac.uk>
2024-11-13 14:41:53 +0000

    Made segments for settings page
    Using this to separate which data needs to go to the server, and which stays local

--- Re-Space-App/app/(tabs)/settings.tsx
+++ Re-Space-App/app/(tabs)/settings.tsx
@@ -12,32 +12,57 @@ const SettingsPage = () => {
             <View style={defaultStyles.pageTitleSection}>
                 <Text style={defaultStyles.pageTitle}>Settings</Text>
             </View>
-            <Text style={defaultStyles.sectionTitle}>Task Settings</Text>
 
-            <SliderSetting label="Movement Speed" min={1} max={3} />
-            <ToggleSetting label="Stop when humans present" />
-            <ActionButton
-                label="Re-map room"
-                onPress={() => alert("Mapping...")}
-            />
+            <View style={uniqueStyles.segmentContainer}>
+                <View style={uniqueStyles.segmentTitleContainer}>
+                    <Text style={uniqueStyles.segmentTitle}>Robot Settings</Text>
+                </View>
 
-            <Text style={defaultStyles.sectionTitle}>Notification Preferences</Text>
+                <SliderSetting label="Movement Speed" min={1} max={3} />
+                <ToggleSetting label="Stop when humans present" />
+                <ActionButton
+                    label="Re-map room"
+                    onPress={() => alert("Mapping...")}
+                />
+            </View>
 
-            <ToggleSetting label="Completed Tasks" />
-            <ToggleSetting label="Collisions" />
-            <ToggleSetting label="Battery Levels" />
-            <SliderSetting label="Battery Notification Threshold" min={1} max={25} />
 
-            <Text style={defaultStyles.sectionTitle}>Analytics and Reporting</Text>
-            <ActionButton
-                label="Task History Logs"
-                onPress={() => alert("Todo...")}
-            />
+            <View style={uniqueStyles.segmentContainer}>
+                <View style={uniqueStyles.segmentTitleContainer}>
+                    <Text style={uniqueStyles.segmentTitle}>App Settings</Text>
+                </View>
+
+
+                <Text style={defaultStyles.sectionTitle}>Notification Preferences</Text>
+                <ToggleSetting label="Completed Tasks" />
+                <ToggleSetting label="Collisions" />
+                <ToggleSetting label="Battery Levels" />
+                <SliderSetting label="Battery Notification Threshold" min={1} max={25} />
+
+                <Text style={defaultStyles.sectionTitle}>Analytics and Reporting</Text>
+                <ActionButton
+                    label="Task History Logs"
+                    onPress={() => alert("Todo...")}
+                />
+            </View>
         </ScrollView>
     );
 };
 
-const styles = StyleSheet.create({
+const uniqueStyles = StyleSheet.create({
+    segmentContainer: {
+        width: '100%',
+        height: 'auto',
+    },
+    segmentTitleContainer: {
+        width: '100%',
+        height: 'auto', // make bigger
+    },
+    segmentTitle: {
+        textAlign: 'center',
+        fontSize: 30,
+        fontWeight: 'bold',
+    }
 });
 
 export default SettingsPage;
