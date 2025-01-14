a07057bb170a01245a40ae087006caa9dd7530b5
RONNY BEN-YEHUDA <rb273414@falmouth.ac.uk>
2025-01-14 11:56:08 +0000

    added manual control

--- Server/wsServer.py
+++ Server/wsServer.py
@@ -10,12 +10,16 @@ import json
 host = "localhost"
 port = 8002
 
+serial_port = "/dev/ttyACM0"
+baud_rate = 9600
 
 # async def echo(websocket):
 #     async for message in websocket:
 #         print(message)
 #         await websocket.send("message is:"  + message)
 
+arduino = serial.Serial(serial_port, baud_rate)
+print(f"connected to arduino, serial port {serial_port}")
 
 async def handle_connection(websocket):
     print("Client connected.")
@@ -24,7 +28,13 @@ async def handle_connection(websocket):
             try:
                 data = json.loads(message)
                 print("Received:", data)
-
+                if data["control"]:
+                    command = data["control"]
+                    print("command is", command)
+                    try:
+                        arduino.write((command + ";").encode())
+                    except:
+                        print("Error in writing to arduino")
                 # Example response
                 response = {"type": "response", "message": "Hello from server!"}
                 await websocket.send(json.dumps(response))
