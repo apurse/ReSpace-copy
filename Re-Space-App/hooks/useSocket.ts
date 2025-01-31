20a109c9a3cd9143df0d998ec61c302d1dc257ff
Alex Purser <AP276620@falmouth.ac.uk>
2025-01-31 10:57:02 +0000

    Moved websocket functionality

--- Re-Space-App/hooks/useSocket.ts
+++ Re-Space-App/hooks/useSocket.ts
@@ -5,5 +5,41 @@ import { useContext } from "react";
 export const useSocket = () => {
   const socket = useContext(SocketContext);
 
+  // connection opened
+  socket.onopen = () => {
+    console.log("WebSocket connection established.");
+  };
+  
+  
+  // a message was received
+  socket.onmessage = (e: { data: string; }) => {
+    try {
+      const data = JSON.parse(e.data);
+      console.log("Received:", data);
+    } catch (err) {
+      console.error("Error parsing message:", err);
+    };
+  };
+  
+  
+  // an error occurred
+  socket.onerror = (e: any) => {console.log(e)};
+  
+  
+  // connection closed
+  socket.onclose = (e: { code: any; reason: any; }) => {console.log(e.code, e.reason)};
+  
+
+  // a message was received
+  socket.onmessage = (e: { data: string; }) => {
+    try {
+      const data = JSON.parse(e.data);
+      console.log("Received:", data);
+    } catch (err) {
+      console.error("Error parsing message:", err);
+    };
+  };
+  
   return socket;
 };
+
