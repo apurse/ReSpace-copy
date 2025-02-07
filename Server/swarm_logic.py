055773ca0a562a71bc102cdbb554118155d6e0f6
Louis-Douglas <contact@LouisDouglas.com>
2025-02-07 11:49:20 +0000

    Add send move task logic to swarm_logic

--- Server/swarm_logic.py
+++ Server/swarm_logic.py
@@ -29,12 +29,26 @@ async def swarm_logic():
         for robot in robots:
             prev = np.inf
             # give first robot closest funiture location
-            for funiture in hub.get_current_furniture_positions:
+            for funiture in hub.get_current_furniture_positions():
                 distance = distance(robot.locationX,robot.locationY,funiture.locationX,funiture.locationY)
                 if distance < prev:
                     closest_funiture = funiture
-                    prev = distance 
-    
+                    prev = distance
+                    for desired in hub.get_desired_furniture_positions():
+                        if desired.id == closest_funiture.id:
+                            desired_furniture = desired
+
+            robot.send_move_task(closest_funiture,desired_furniture)
+
+# Testing the node com_handler connection
+# async def test():
+#     await asyncio.sleep(10)
+#     while True:
+#         for robot in hub.get_connected_robots():
+#             print(f"Sending message \"Hello World\" to {robot.id}")
+#             await robot.send_custom_data("Hello World")
+#         await asyncio.sleep(10)
+
 
 def distance(x1,y1,x2,y2):
     return np.sqrt(np.square(x2 - x1) + np.square(y2 - y1))
@@ -42,8 +56,8 @@ def distance(x1,y1,x2,y2):
 
 async def main():
     hub_communication_task = asyncio.create_task(hub.main())  # Start the WebSocket server
-    swarm_task = asyncio.create_task(swarm_logic())  # Run the swarm logic
-
+    # swarm_task = asyncio.create_task(swarm_logic())  # Run the swarm logic
+    swarm_task = asyncio.create_task(test())
     try:
         await asyncio.gather(hub_communication_task, swarm_task)  # Run both tasks concurrently
     except asyncio.CancelledError:
