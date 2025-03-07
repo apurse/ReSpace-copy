732c56be71570566d01aa7533f567b3ea9b3ca3c
Alex Purser <AP276620@falmouth.ac.uk>
2025-03-07 13:16:39 +0000

    Made QR generator and encryptor based off Aruco work

--- Machine-Vision-Scripts/ArucoMarkerGenerator.py
+++ Machine-Vision-Scripts/ArucoMarkerGenerator.py
@@ -1,26 +1,26 @@
-import cv2
-import cv2.aruco as aruco
+import qrcode
+import base64
 
-# Set the ArUco dictionary (you can choose others, e.g., DICT_4X4_50)
-aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_6X6_250)
+saving = True
 
-# Input the ID to encode in the marker
-marker_id = int(input("Enter the ID for the ArUco marker (0 - 249): "))
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=1,
+)
 
-# Check if the ID is within the range of the dictionary
-if 0 <= marker_id < 250:
-    # Generate the marker
-    marker_size = 200  # Marker size in pixels
-    marker_image = aruco.generateImageMarker(aruco_dict, marker_id, marker_size)
+def generateQRCode(data):
 
-    # Save the marker image
-    filename = f"aruco_marker_id_{marker_id}.png"
-    cv2.imwrite(filename, marker_image)
-    print(f"Aruco marker with ID {marker_id} saved as {filename}")
+    # Generate image
+    qr.add_data(data)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+    if saving == True:
+        img.save("qrcode.png")
+    
+    # Encode into base64
+    with open("qrcode.png", "rb") as image_file:
+        encodedString = base64.b64encode(image_file.read())
 
-    # Optionally, display the generated marker
-    cv2.imshow("Generated ArUco Marker", marker_image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-else:
-    print("Invalid ID! Please enter a number between 0 and 249.")
+    return encodedString
