0e31307ac6a2d5893f3c427abaeaa334811ed7dc
Alex Purser <AP276620@falmouth.ac.uk>
2025-03-09 16:32:56 +0000

    Minor visual changes to addFurniture. Printing not possible

--- Re-Space-App/app/addPages/addFurniture.tsx
+++ Re-Space-App/app/addPages/addFurniture.tsx
@@ -8,6 +8,8 @@ import * as FileSystem from 'expo-file-system';
 import Furniture from '@/components/LayoutComponents/furniture';
 import { ColourPickerModal } from '@/components/LayoutComponents/colourPickerModal';
 import { useSocket } from "@/hooks/useSocket";
+import { red } from 'react-native-reanimated/lib/typescript/Colors';
+
 
 
 // Local json file with furniture data
@@ -56,39 +58,16 @@ export default function AddLayout() {
   const [length, setLength] = useState<number>(0);
   const [quantity, setQuantity] = useState<number>(0);
   const [selectedColour, setSelectedColour] = useState<string>('#aabbcc');
-  // const [QRData, setQRData] = useState<string>('');
+  const [QRData, setQRData] = useState<string>('');
   const { socket, isConnected, robotData, sendMessage, QRCode } = useSocket();
 
 
   // State of furniture modal (to add furniture)
   const [isModalVisible, setModalVisible] = useState(false);
 
-  var addQRCode: any = [];
-
-  // When the QRcode has been recieved
-  if (QRCode != null && addQRCode.length == 0) {
-    // setQRData(QRCode);
-    
-    addQRCode.push(
-      <View style={{ alignItems: 'center' }}>
-        {/* QR generation */}
-        <View style={uniqueStyles.pngContainer}>
-          <Image
-            style={uniqueStyles.imageBody}
-            source={{ uri: (`data:image/png;base64, ${QRCode}`) }} />
-        </View>
-        {/* Print button */}
-        <View style={uniqueStyles.buttonContainer}>
-          {notifications && <Text style={uniqueStyles.notificationText}>{notifications}</Text>}
-          <ActionButton
-            label="Print QR Code"
-            onPress={alert}
-            />
-        </View>
-      </View>
-    )
+  const downloadQR = async () => {
+    // download image locally
   }
-
   const saveFurniture = async () => {
 
     if (!name || !heightF || !widthF || !length || !quantity) {
@@ -167,11 +146,11 @@ export default function AddLayout() {
       setLength(0);
       setQuantity(0);
       setSelectedColour('#aabbcc');
-      // setQRData('');
+      setQRData('');
       console.log(addQRCode.length)
       addQRCode.pop();
       console.log(addQRCode.length)
-      
+
     }
     catch (error) {
       console.error('Failed to update/save data to json file:', error);
@@ -184,6 +163,32 @@ export default function AddLayout() {
     }
   };
 
+  var addQRCode: any = [];
+
+  // When the QRcode has been recieved
+  if (QRCode != null && addQRCode.length == 0) {
+    setQRData(QRCode);
+
+    addQRCode.push(
+      <View style={{ alignItems: 'center' }}>
+        {/* QR generation */}
+        <View style={uniqueStyles.pngContainer}>
+          <Image
+            style={uniqueStyles.imageBody}
+            source={{ uri: (`data:image/png;base64, ${QRCode}`) }} />
+        </View>
+        {/* Print button */}
+        <View style={uniqueStyles.buttonContainer}>
+          {notifications && <Text style={uniqueStyles.notificationText}>{notifications}</Text>}
+          <ActionButton
+            label="Print QR Code"
+            onPress={downloadQR}
+          />
+        </View>
+      </View>
+    )
+  }
+
   return (
     <ScrollView contentContainerStyle={defaultStyles.body}>
 
@@ -192,86 +197,100 @@ export default function AddLayout() {
         <Text style={defaultStyles.pageTitle}>Add Furniture</Text>
       </View>
 
-      <View style={uniqueStyles.inputField}>
-        <Text style={uniqueStyles.inputHeader}>Name</Text>
-        <TextInput
-          value={name}
-          onChangeText={setName}
-          style={uniqueStyles.textInput}
-          placeholder='*Enter name...*'
-        />
+      <View style={uniqueStyles.inputRow}>
+        <View style={uniqueStyles.inputField}>
+          <Text style={uniqueStyles.inputHeader}>Name</Text>
+          <TextInput
+            value={name}
+            onChangeText={setName}
+            style={uniqueStyles.textInput}
+            placeholder='*Enter name...*'
+          />
+        </View>
+        <View style={uniqueStyles.inputField}>
+          <Text style={uniqueStyles.inputHeader}>Model</Text>
+          <TextInput
+            value={model}
+            onChangeText={setModel}
+            style={uniqueStyles.textInput}
+            placeholder='Enter model type...'
+          />
+        </View>
       </View>
-      <View style={uniqueStyles.inputField}>
-        <Text style={uniqueStyles.inputHeader}>Model</Text>
-        <TextInput
-          value={model}
-          onChangeText={setModel}
-          style={uniqueStyles.textInput}
-          placeholder='Enter model type...'
-        />
+
+
+      <View style={uniqueStyles.inputRow}>
+        <View style={uniqueStyles.inputField}>
+          <Text style={uniqueStyles.inputHeader}>Height</Text>
+          <TextInput
+            // If there is nothing ('0') then show empty string (to keep placeholder)
+            value={heightF ? heightF.toString() : ''}
+            // Check update value with a number
+            onChangeText={(text) => setHeight(Number(text))}
+            style={uniqueStyles.textInput}
+            placeholder='*Enter height value...*'
+            keyboardType="numeric"
+          />
+        </View>
+        <View style={uniqueStyles.inputField}>
+          <Text style={uniqueStyles.inputHeader}>Width</Text>
+          <TextInput
+            // If there is nothing ('0') then show empty string (to keep placeholder) 
+            value={widthF ? widthF.toString() : ''}
+            // Check update value with a number
+            onChangeText={(text) => setWidth(Number(text))}
+            style={uniqueStyles.textInput}
+            placeholder='*Enter width value...*'
+            keyboardType="numeric"
+          />
+        </View>
       </View>
-      <View style={uniqueStyles.inputField}>
-        <Text style={uniqueStyles.inputHeader}>Height</Text>
-        <TextInput
-          // If there is nothing ('0') then show empty string (to keep placeholder)
-          value={heightF ? heightF.toString() : ''}
-          // Check update value with a number
-          onChangeText={(text) => setHeight(Number(text))}
-          style={uniqueStyles.textInput}
-          placeholder='*Enter height value...*'
-          keyboardType="numeric"
-        />
+
+      <View style={uniqueStyles.inputRow}>
+        <View style={uniqueStyles.inputField}>
+          <Text style={uniqueStyles.inputHeader}>Length</Text>
+          <TextInput
+            // If there is nothing ('0') then show empty string (to keep placeholder)
+            value={length ? length.toString() : ''}
+            // Check update value with a number 
+            onChangeText={(text) => setLength(Number(text))}
+            style={uniqueStyles.textInput}
+            placeholder='*Enter length value...*'
+            keyboardType="numeric"
+          />
+        </View>
+        <View style={uniqueStyles.inputField}>
+          <Text style={uniqueStyles.inputHeader}>Quantity</Text>
+          <TextInput
+            // If there is nothing ('0') then show empty string (to keep placeholder)
+            value={quantity ? quantity.toString() : ''}
+            // Check update value with a number
+            onChangeText={(text) => setQuantity(Number(text))}
+            style={uniqueStyles.textInput}
+            placeholder='*Enter quantity value...*'
+            keyboardType="numeric"
+          />
+        </View>
       </View>
-      <View style={uniqueStyles.inputField}>
-        <Text style={uniqueStyles.inputHeader}>Width</Text>
-        <TextInput
-          // If there is nothing ('0') then show empty string (to keep placeholder) 
-          value={widthF ? widthF.toString() : ''}
-          // Check update value with a number
-          onChangeText={(text) => setWidth(Number(text))}
-          style={uniqueStyles.textInput}
-          placeholder='*Enter width value...*'
-          keyboardType="numeric"
-        />
-      </View>
-      <View style={uniqueStyles.inputField}>
-        <Text style={uniqueStyles.inputHeader}>Length</Text>
-        <TextInput
-          // If there is nothing ('0') then show empty string (to keep placeholder)
-          value={length ? length.toString() : ''}
-          // Check update value with a number 
-          onChangeText={(text) => setLength(Number(text))}
-          style={uniqueStyles.textInput}
-          placeholder='*Enter length value...*'
-          keyboardType="numeric"
-        />
-      </View>
-      <View style={uniqueStyles.inputField}>
-        <Text style={uniqueStyles.inputHeader}>Quantity</Text>
-        <TextInput
-          // If there is nothing ('0') then show empty string (to keep placeholder)
-          value={quantity ? quantity.toString() : ''}
-          // Check update value with a number
-          onChangeText={(text) => setQuantity(Number(text))}
-          style={uniqueStyles.textInput}
-          placeholder='*Enter quantity value...*'
-          keyboardType="numeric"
-        />
-      </View>
-      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+
+
+      <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 20 }}>
         {/* <Text style={uniqueStyles.inputHeader}>Colour</Text> */}
         <ActionButton
           label="Pick a colour"
-          onPress={() => setModalVisible(true)}
           style={{ right: 15 }}
+          onPress={() => setModalVisible(true)}
         />
+
+        <Text style={[uniqueStyles.colourSelected, { backgroundColor: selectedColour }]}></Text>
+
+        {/* Modal popup */}
         <ColourPickerModal
           isVisible={isModalVisible}
           onClose={() => setModalVisible(false)}
           selectedColour={selectedColour}
           onSelectedColour={setSelectedColour}
         />
-        <Text style={[uniqueStyles.colourSelected, { backgroundColor: selectedColour }]}></Text>
       </View>
 
       <View style={uniqueStyles.buttonContainer}>
@@ -296,14 +315,26 @@ const createUniqueStyles = (isDarkMode: boolean) =>
     inputHeader: {
       fontSize: 16,
       color: isDarkMode ? '#fff' : '#333',
+      paddingBottom: 5,
+    },
+    inputRow: {
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+      justifyContent: 'space-between',
+      // gap: width * 0.01,
+      width: '100%',
+      paddingBottom: 20,
+      alignItems: 'center'
     },
     textInput: {
-      width: width * 0.5,
+      width: width * 0.43,
+      borderRadius: 4,
+      paddingLeft: 4,
       flexDirection: 'row',
       justifyContent: 'space-between',
       alignItems: 'center',
       height: 40,
-      backgroundColor: isDarkMode ? '#fff' : '#000',
+      backgroundColor: isDarkMode ? 'white' : 'black',
       color: isDarkMode ? '#000' : '#fff',
       fontSize: 14,
       fontWeight: 'bold',
@@ -328,16 +359,18 @@ const createUniqueStyles = (isDarkMode: boolean) =>
       zIndex: 1000,
     },
     colourSelected: {
-      padding: 12,
+      padding: 20,
       marginTop: 20,
       width: 50,
+      height: 50,
+      borderRadius: 30
     },
     pngContainer: {
       marginTop: 20,
       width: 200,
       borderColor: isDarkMode ? '#fff' : '#ddd',
       borderWidth: 1,
-      height: 200,
+      height: 200 * 1.5,
       alignItems: 'center',
     },
     imageBody: {
