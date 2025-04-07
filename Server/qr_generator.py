64d6f04d2c795c3249fdddc5ac8dbf6e8b5e95d8
Alex Purser <AP276620@falmouth.ac.uk>
2025-04-07 15:38:06 +0100

    QR codes now add name and id to bottom
    Each piece of furniture has its provided name and ID, atm the ID has not been set, debating wether it should be unique per furniture or furniture type.

--- Server/qr_generator.py
+++ Server/qr_generator.py
@@ -3,9 +3,11 @@ import base64
 from io import BytesIO
 import cv2
 import numpy as np
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 
 # https://stackoverflow.com/questions/75152880/cannot-convert-np-array-from-bool-to-uint8-and-save-image-properly-with-opencv
+# https://stackoverflow.com/questions/4902198/pil-how-to-scale-text-size-in-relation-to-the-size-of-the-image
+
 
 saving = False
 
@@ -16,8 +18,16 @@ qr = qrcode.QRCode(
     border=1,
 )
 
-# Make arrow image
+
 def generateArrow(qrCode):
+    """
+    Generate the arrow part of the qr code image.
+    Args:
+        qrCode (str): The cv2 qrCode 8-bit array.
+
+    Returns:
+        arrow [cv2 image]: The arrow image.
+    """
     arrow = Image.new('RGB', (qrCode.shape[0], int(qrCode.shape[1] / 1.5)), (255,255,255))
     arrow = np.array(arrow)
 
@@ -36,8 +46,15 @@ def generateArrow(qrCode):
     return arrow
 
 
-
 def generateQRCode(data):
+    """
+    Generate the qr code image from the furniture data.
+    Args:
+        data (JSON): The furniture data stored as a JSON file.
+
+    Returns:
+        encodedString [base64 string]: The QRCode image encoded into base64.
+    """
 
     # Generate QR code image
     qr.add_data(data)
@@ -58,21 +75,43 @@ def generateQRCode(data):
     combined = np.vstack((arrow, qrCode))
     combined = Image.fromarray(combined)
     
+    
+    fontSize = 30
+    name = (data["name"] + "_" + data["id"])
+    
+    
+    # size relative to image
+    img_fraction = 0.95
+    font = ImageFont.truetype("arial.ttf", fontSize)
+    
+    
+    # Decrease font size until the text fits
+    while (font.getbbox(name)[2]) > (img_fraction * combined.width):
+        fontSize -= 1
+        font = ImageFont.truetype("arial.ttf", fontSize)
+
+
+    # Create a background and add the combined image with text
+    finalImage = Image.new('RGBA', (combined.width, combined.height + fontSize + 5), (255,255,255,255))
+    draw = ImageDraw.Draw(finalImage)
+    finalImage.paste(combined, (0,0))
+    draw.text(((combined.width - font.getbbox(name)[2]) / 2, combined.height - 5), name, (0,0,0), font=font)
+
 
     if saving:
         print("Saving images to this directory")
         Image.fromarray(qrCode).save("qrImage.png")
         Image.fromarray(arrow).save("arrow.png")
-        combined.save("combined.png")
+        finalImage.save('final.png')
 
 
     # Change to buffer
     buffered = BytesIO()
-    combined.save(buffered, format="PNG")
+    finalImage.save(buffered, format="PNG")
     encodedString = base64.b64encode(buffered.getvalue()).decode("utf-8")
     # print(encodedString)
     qr.clear()
 
     return encodedString
 
-# generateQRCode('{"name":"test"}')
+#generateQRCode({"name":"Black Chair", "id":"2"})
