d09ba43ff027beb6ae94febdcbdfff6092e8c4b8
Alex Purser <AP276620@falmouth.ac.uk>
2025-04-12 18:12:54 +0100

    Added more sign-in functionality.
    Added notifications and a checklist of whats needed for making it run. Saved useful code snippets that could be used in account creation and deletion. Might use sqlite instead of json, unsure as of yet.

--- Re-Space-App/app/(tabs)/sign-in.tsx
+++ Re-Space-App/app/(tabs)/sign-in.tsx
@@ -3,12 +3,34 @@ import React, { useEffect, useState } from 'react';
 import { createDefaultStyles } from '@/components/defaultStyles';
 import { useTheme } from '@/app/_layout';
 import ActionButton from '@/components/settingsComponents/actionButton';
-import furnitureData from '@/Jsons/FurnitureData.json';
 import * as FileSystem from 'expo-file-system';
 import { useSocket } from "@/hooks/useSocket";
-import * as MediaLibrary from 'expo-media-library';
 
 
+
+// Useful for deleting account
+    // // Call function to clear the json data from device
+    // const clearJsonData = async () => {
+    //   try {
+    //     await FileSystem.writeAsStringAsync(localJson, JSON.stringify({ Furniture: [] }));
+
+    //     console.log('Data has been cleared');
+    //   } catch (error) {
+    //     console.error('Error deleting json data');
+    //   }
+    // };
+
+
+// Could be used for creating user
+    // // Adding data to json
+    // const updateData = [...jsonData.Furniture, userConnected];
+
+    // // Write new data to json
+    // await FileSystem.writeAsStringAsync(localJson, JSON.stringify({ Furniture: updateData }));
+
+    // // Show local json file in console
+    // const data = await FileSystem.readAsStringAsync(localJson);
+    // // console.log('Furniture json updated:', data);
 
 // Get dimensions of the screen
 const { width, height } = Dimensions.get('window');
@@ -22,8 +44,51 @@ export default function SignIn() {
 
   const [username, setUsername] = useState<string>('');
   const [password, setPassword] = useState<string>('');
-  const { socket, isConnected, robotData, sendMessage, QRCode } = useSocket();
+  const { socket, isConnected, sendMessage } = useSocket();
 
+  const [notifications, setnotifications] = useState<string | null>(null);
+
+
+  const authorise = async () => {
+
+    if (!username || !password) {
+      // Show notifications to fill the fields
+      setnotifications('Please fill the necessary fields (Marked with \'*\')');
+
+      // Show notifications for 3 sec
+      setTimeout(() => setnotifications(null), 3000);
+
+      return;
+    }
+
+    const userConnected = {
+      username,
+      password,
+    };
+
+    try {
+
+
+      // if user is in db:
+      //   connect as that user
+      //   on main page have "good morning ${user}
+      //   add an account bit in settings
+
+
+      // Tell server user has connected (optional)
+      if (isConnected) {
+        sendMessage({ type: "account_login", data: userConnected });
+      } else {
+        alert("No connection to the WebSocket.");
+      }
+
+    }
+    catch (error) {
+      console.error('Failed to login', error);
+      setnotifications('Failed to login.')
+      setTimeout(() => setnotifications(null), 3000);
+    }
+  };
 
   return (
     <ScrollView contentContainerStyle={defaultStyles.body}>
@@ -53,6 +118,7 @@ export default function SignIn() {
         />
       </View>
 
+      {notifications && <Text style={uniqueStyles.notificationText}>{notifications}</Text>}
       <ActionButton
         label="Forgot password?"
         onPress={() => alert("WIP")}
@@ -62,7 +128,7 @@ export default function SignIn() {
 
       <ActionButton
         label="Login"
-        onPress={() => alert("WIP")}
+        onPress={authorise}
         style={{ width: '80%' }}
       />
 
@@ -105,7 +171,6 @@ const createUniqueStyles = (isDarkMode: boolean) =>
       fontFamily: 'arial',
     },
     forgotPassword: {
-      // alignItems: 'baseline',
       padding: 0,
       backgroundColor: 'null',
       textDecorationColor: '#269bd1',
@@ -117,8 +182,22 @@ const createUniqueStyles = (isDarkMode: boolean) =>
       alignItems: 'center',
       textAlign: 'center',
       padding: 20,
-      // verticalAlign: 'bottom',
       marginTop: 100,
       backgroundColor: 'null',
+    },
+    notificationText: {
+      position: 'absolute',
+      top: 100,
+      left: 0,
+      right: 0,
+      backgroundColor: isDarkMode ? 'rgba(255,255, 255, 0.7)' : 'rgba(0,0, 0, 0.7)',
+      color: isDarkMode ? '#000' : '#fff',
+      padding: 10,
+      borderRadius: 5,
+      marginBottom: 15,
+      fontSize: 16,
+      fontWeight: 'bold',
+      textAlign: 'center',
+      zIndex: 1000,
     },
   });
