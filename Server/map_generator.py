633723f2c80cbd1d707f2cb2d2c0d9aa33f0a8fc
Alex Purser <AP276620@falmouth.ac.uk>
2025-04-23 17:39:42 +0100

    Added a .pgm to .png converter in comms_hub
    Since the data will be parsed across as a .pgm file, it needs to be converted to .png and parsed over as base64 to be seen in the app. This script takes the .pgm, reformats it as a .png, outputs the base64 string, and sends it over to the hub.

---
+++ Server/map_generator.py
@@ -0,0 +1,46 @@
+import os
+import base64
+from PIL import Image
+
+
+def convertToPNG(pgmFile):
+    """
+    Converts the .pmg map file into a .png file and base64 string.
+    Args:
+        link (str): The file path for the .pmg map.
+
+    Returns:
+        encodedString [string]: The .png image converted into a base64 string.
+    """
+    
+    # Get the file and format it as .png
+    filename, extension  = os.path.splitext(pgmFile)
+    pngFile = "{}.png".format(filename)
+    
+    # Save image into directory as png
+    with Image.open(pgmFile) as im:
+        im.save(pngFile)
+    
+    # Encode the saved image into base64
+    with open(pngFile, "rb") as image_file:
+        encodedString = base64.b64encode(image_file.read())
+
+    print(encodedString)
+    remove_img(pngFile)
+
+    return encodedString
+
+
+def remove_img(pngFile):
+    """
+    Remove the image saved from the directory
+    Args:
+        pngFile (str): The file path for the .png image.
+    """
+    
+    # check if the file exists and remove
+    if os.path.exists(pngFile):
+        os.remove(pngFile)
+        print("Removed!")
+
+# convertToPNG(r'outside_lab.pgm')
